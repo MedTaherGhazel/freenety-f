cf3edf997752cc9f7acef7a9efc9d96e01091d72
Med Taher Ghazel <119009624+MedTaherGhazel@users.noreply.github.com>
2023-08-11 10:32:23 +0100

    banner

---
+++ src/app/banner/banner.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-banner',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss']
+})
+export class BannerComponent {
+
+}
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -3,5 +3,12 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Injectable()
 export class UserService {
+    logout: any;
+    refreshToken() {
+      throw new Error('Method not implemented.');
+    }
+    login(arg0: { username: any; password: any; }) {
+      throw new Error('Method not implemented.');
+    }
     constructor(private http: HttpClient) {}
 }
